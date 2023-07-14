6ac7ade947b9faadda1e796d206f92e50aaca2ff
xuanco941 <xuanco941@gmail.com>
2023-07-14 11:43:58 +0700

    repair

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Customizer from './Customizer';
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
+import CameraRig from './CameraRig';
+
+import Shirt from './Shirt';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <main>
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 0], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+        className="w-full max-w-full h-full transition-all ease-in"
+      >
+        <ambientLight intensity={0.5} />
+        <Environment preset="city" />
+
+        <CameraRig>
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+      </Canvas>
+      <Customizer />
+    </main>
   </React.StrictMode>,
 )
--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,8 @@
 import { proxy } from 'valtio';
 
 const state = proxy({
-  intro: true,
   color: '#EFBD48',
-  isLogoTexture: true,
-  isFullTexture: false,
-  logoDecal: './threejs.png',
+  isFullTexture: true,
   fullDecal: './threejs.png',
 });
 
